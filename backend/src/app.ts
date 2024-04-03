81bef5b0d9b21a2928f4fd54cf97adc04747916c
Erick Henriquez <erickdavid7@hotmail.com>
2024-04-02 19:27:37 -0500

    started express implementation

---
+++ backend/src/app.ts
@@ -0,0 +1,44 @@
+import express  from "express";
+import cors from "cors";
+import { port } from "./utils/constants";
+
+//* ----------------Server configuration -----------------
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, DELETE'
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send(`
+  <h1>Welcome to Neuma's backend server</h1>
+  <h2>Code: 200</h2>
+  <p>Good request</p>
+  `);
+});
+
+app.listen(port, () =>{
+  // local testing
+  console.log(`Server is listening at http://localhost:${port}/`);
+  // For the deployment
+  // console.log(`Server is listening at https://neumapp.site:${port}/`);
+});
