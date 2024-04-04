ae9c9aa2cf66c89cb4feca4bd5165207ecce5cf2
Erick Henriquez <erickdavid7@hotmail.com>
2024-04-03 20:14:36 -0500

    improved comment

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,5 +5,5 @@ dotenv.config();
 
 export const port: string | undefined = process.env.PORT;
 
-// It will go back 3 folder levels from this file's directory in order to go to ssl's folder that is supposed to be outside of the project's directory
+// It will go back 3 folder levels from this file's directory in order to reach ssl's folder path that is supposed to be outside of the project's directory
 export const sslPathOutsideRep: string | undefined = path.join(__dirname, "..", "..", "..", "/ssl");
