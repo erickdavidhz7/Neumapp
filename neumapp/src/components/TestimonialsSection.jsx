82f0e5dfb12f806307a084b35d01af136a6127a6
EdwinCoder85 <edwinagemiler17@gmail.com>
2024-04-08 10:12:31 -0500

    se quito la clase mt-4 del componente TestimonialsSection.jsx

--- neumapp/src/components/TestimonialsSection.jsx
+++ neumapp/src/components/TestimonialsSection.jsx
@@ -3,7 +3,7 @@ import TestimonialCard from "./TestimonialCard";
 export default function TestimonialsSection() {
   return (
     <section id="testimonials" className="bg-zinc-700">
-      <h2 className="text-center text-5xl font-bold text-white mt-4 mb-8 md:mb-12">
+      <h2 className="text-center text-5xl font-bold text-white mb-8 md:mb-12">
         Testimonios
       </h2>
       <div className="container mx-auto w-full max-w-sm md:max-w-3xl lg:mx-0 lg:max-w-6xl">
