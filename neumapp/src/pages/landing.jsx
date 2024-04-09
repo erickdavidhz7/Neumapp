544499448346cb2b1b065814b7bd36fdd26a3ab7
lupattin <lupattin@gmail.com>
2024-04-08 21:03:22 -0300

    fix login - delete package

--- neumapp/src/pages/landing.jsx
+++ neumapp/src/pages/landing.jsx
@@ -7,11 +7,13 @@ import NavBar from "../components/NavBar";
 import LandingOne from "../components/landingOne";
 import LandingTwo from "../components/landingTwo";
 import Servicios from "../components/Servicios";
+import Generalogin from "../components/Login/Generalogin";
 
 export default function Landing() {
   return (
     <main className="bg-zinc-700">
       <div className="w-[90%] max-w-6xl mx-auto">
+        <Generalogin></Generalogin>
         <NavBar />
         <LandingOne />
         <LandingTwo />
