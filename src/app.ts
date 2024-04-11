bdf6e10d59265bffe55ab93ee406b038be77da59
Jorge Trujillo <jorge.trujillo@epn.edu.ec>
2024-04-10 23:17:49 -0500

    Update code in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,78 +2,32 @@ import express from 'express'
 import cors from 'cors'
 import https from 'node:https'
 import http from 'node:http'
-import fs from 'fs'
 import swaggerUi from 'swagger-ui-express'
-import YAML from 'yamljs'
-import { envs, sslPathOutsideRep } from './utils/constants'
+import { corsOptions, envs, httpsOptions } from './utils/constants'
 import routes from './routes/router'
-import { db } from './utils/database'
+import { initDb } from './utils/database'
 import { initModels } from './models/initModels'
+import openApiSpec from '../openapiDoc/openapi.json'
 
 //* ----------------Server configuration -----------------
-
-const openApiSpec = YAML.load('openapiDoc/openapi.yaml')
 const app = express()
 
 app.use(express.json())
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-  })
-)
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(cors(corsOptions))
 
-db.authenticate()
-  .then(() => {
-    console.log('Database Authenticated')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
-db.sync()
-  .then(() => {
-    console.log('Database Synced')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
+initDb()
 initModels()
-
-app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
-})
 
 //Llamo al router principal
 app.use('/', routes)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiSpec))
 
-https
-  .createServer(
-    {
-      cert: fs.readFileSync(sslPathOutsideRep + '/fullchain.pem'),
-      key: fs.readFileSync(sslPathOutsideRep + '/privkey.pem'),
-    },
-    app
-  )
-  .listen(envs.PORT, () => {
-    // local testing
-    // console.log(`Server is listening at https://localhost:${envs.PORT}/`)
-    //For the deployment
-    console.log(`Server is listening at https://neumapp.site:${envs.PORT}/`)
-  })
-/* 
-http
-  .createServer(app)
-  .listen(8000, () => {
-    console.log(`Server is listening at http://localhost:${8000}/`)
-  })
- */
+https.createServer(httpsOptions, app).listen(envs.PORT, () => {
+  console.log(`Server is listening at https://${envs.APP_DOMAIN}:${envs.PORT}/`)
+})
+/*
+http.createServer(app).listen(8000, () => {
+  console.log(`Server is listening at http://${envs.APP_DOMAIN}:${8000}/`)
+})
+*/
