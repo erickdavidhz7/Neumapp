15badb245d2990c85c447ded8973ba3e6b1d3af1
Jorge Trujillo <jorge.trujillo@epn.edu.ec>
2024-04-10 21:07:13 -0500

    Jwt middleware

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -6,21 +6,13 @@ import { envs } from '../utils/constants'
 export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body
-
     if (!email || !password) {
       return res.status(400).json({ message: 'Missing Data' })
     }
 
-    const user = await loginUser(email, password)
+    const token = await loginUser(email, password)
 
-    if (user) {
-      const token = jwt.sign(
-        {
-          email: user.email,
-        },
-        envs.JWT_SECRET
-      )
-
+    if (token) {
       return res.status(200).json({ message: 'User Logged', token })
     } else {
       res.status(401).json({ message: 'Invalid Credentials' })
