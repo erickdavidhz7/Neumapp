fd1f575906b3b571996ad3a590532fc304a05e54
michqiueli <michiqueli@gmail.com>
2024-04-12 20:41:27 -0300

    correccion de errores menors en nombres de rutas

--- src/routes/providers.routes.ts
+++ src/routes/providers.routes.ts
@@ -5,9 +5,9 @@ const router = express.Router()
 //---- ACA COMIENZAN LOS ENDPOINT DE USUARIOS ----/
 
 router.get('/', ProvidersControllers.getAllProviders)
+router.post('/', ProvidersControllers.createProvider)
 router.get('/:id', ProvidersControllers.getProviderById)
-router.post('/createProvider', ProvidersControllers.createProvider)
-router.patch('updateProvider/:id', ProvidersControllers.updateProvider)
+router.patch('/:id', ProvidersControllers.updateProvider)
 
 //Aca debemos agregar las demas rutas de usuarios
 
--- src/routes/reviews.routes.ts
+++ src/routes/reviews.routes.ts
@@ -3,9 +3,9 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/', ReviewsControllers.getAllReviews)
+router.post('/', ReviewsControllers.createReview)
+router.patch('/:id', ReviewsControllers.updateReview)
 router.get('/UserId/:id', ReviewsControllers.getByUserId)
 router.get('/ProviderId/:id', ReviewsControllers.getByProviderId)
-router.post('/createReview', ReviewsControllers.createReview)
-router.patch('/updateReview/:id', ReviewsControllers.updateReview)
 
 export default router
