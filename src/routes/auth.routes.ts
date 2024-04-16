22ccc22493ad75137bea48efd131b6b09e46a99a
Jorge Trujillo <jorge.trujillo@epn.edu.ec>
2024-04-15 22:54:55 -0500

    middlewares addes on routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,22 @@
 import express from 'express'
 import UsersControllers from '../controllers/users.controllers'
 import { login } from '../controllers/auth.controllers'
+import {
+  signinProviderValidator,
+  signinUserValidator,
+} from '../middlewares/auth.middleware'
 const router = express.Router()
 
 router.post('/login', login)
-router.post('/register/client', UsersControllers.registerClient)
-router.post('/register/provider', UsersControllers.registerProvider)
+router.post(
+  '/register/client',
+  [signinUserValidator],
+  UsersControllers.registerClient
+)
+router.post(
+  '/register/provider',
+  [signinProviderValidator],
+  UsersControllers.registerProvider
+)
 
 export default router
--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -1,10 +1,19 @@
 import OrdersControllers from '../controllers/order.controller'
 import express from 'express'
+import { uudiV4Validator } from '../middlewares'
 const router = express.Router()
 
 router.get('/', OrdersControllers.getAllOrders)
-router.get('/UserId/:id', OrdersControllers.getByUserId)
-router.get('/ProviderId/:id', OrdersControllers.getByProviderId)
+router.get(
+  '/UserId/:id',
+  [uudiV4Validator('id')],
+  OrdersControllers.getByUserId
+)
+router.get(
+  '/ProviderId/:id',
+  [uudiV4Validator('id')],
+  OrdersControllers.getByProviderId
+)
 router.post('/', OrdersControllers.createOrder)
 router.patch('/:id', OrdersControllers.updateOrder)
 
