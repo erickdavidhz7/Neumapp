54eb33c6cbc108e5f720d9afc45ed8e51ff0a05a
michqiueli <michiqueli@gmail.com>
2024-04-19 20:34:46 -0300

    Integracion de mercvado pago... no pude probar porque el servidor no acepta conexiones desde http

--- neumapp/src/components/UI/Button/Button.jsx
+++ neumapp/src/components/UI/Button/Button.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React from "react";
 import { Button } from "@nextui-org/react";
 
 const ButtonUI = ({ children, ...props }) => {
+  const data = {
+    items: [
+      {
+        title: "Reparacion",
+        quantity: 1,
+        unit_price: 10,
+        description: "",
+      },
+    ],
+    payer: {
+      name: "Nicolas",
+      email: "michiqueli@gmail.com",
+    },
+  };
+  const pedirServicio = async (data) => {
+    const postPayment = async (data) => {
+      try {
+        const response = await fetch(`https://localhost:3001/payments`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
+        const responseData = await response.json();
+        return responseData;
+      } catch (error) {
+        if (error instanceof Error) {
+          window.alert(error.message);
+        } else {
+          window.alert("Ocurrió un error, reintente por favor.");
+        }
+      }
+    };
+    await postPayment(data).then((responseData) => {
+      window.location.href = responseData.result.init_point;
+    });
+  };
+
   return (
-    <Button {...props}>
+    <Button {...props} onClick={() => pedirServicio(data)}>
       {children}
     </Button>
   );
 };
 
 export default ButtonUI;
-
