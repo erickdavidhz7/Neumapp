125bcaa08037ac6c8e880a99c825e56a3f1864fa
Jorge Trujillo <jorge.trujillo@epn.edu.ec>
2024-04-23 00:38:31 -0500

    Update orders.services.ts

--- src/services/orders.services.ts
+++ src/services/orders.services.ts
@@ -61,44 +61,44 @@ const ordersServices = {
         throw { status: 404, msg: 'Provider does not offers the service' }
       }
       orderData.ProviderServiceId = serviceProvider.dataValues.id
-      const newOrder = await createOrder(orderData)
+      const newOrder = await ordersServices.createOrder(orderData)
       return newOrder
     } catch (error) {
       throw error
     }
   },
+  createOrder:async (orderData: OrderI) => {
+    try {
+      if (
+        !orderData.ProviderId ||
+        !orderData.UserId ||
+        !orderData.latitude ||
+        !orderData.longitude ||
+        !orderData.ProviderServiceId
+      ) {
+        throw new Error('Missing Data' as string)
+      }
+      const randomSixDigitNumber = Math.floor(100000 + Math.random() * 900000)
+      console.log(orderData)
+  
+      const newOrder = await Orders.create(
+        {
+          type: 'delivery',
+          latitude: orderData.latitude,
+          longitude: orderData.longitude,
+          code: randomSixDigitNumber,
+          description: orderData.description || '',
+          status: 'started',
+          UserId: orderData.UserId,
+          ProviderId: orderData.ProviderId,
+          ProviderServiceId: orderData.ProviderServiceId,
+        }
+      )
+      return newOrder
+    } catch (error) {
+      throw new Error(error as string)
+    }
+  }
 }
 export default ordersServices
 
-const createOrder = async (orderData: OrderI) => {
-  try {
-    if (
-      !orderData.ProviderId ||
-      !orderData.UserId ||
-      !orderData.latitude ||
-      !orderData.longitude ||
-      !orderData.ProviderServiceId
-    ) {
-      throw new Error('Missing Data' as string)
-    }
-    const randomSixDigitNumber = Math.floor(100000 + Math.random() * 900000)
-    console.log(orderData)
-
-    const newOrder = await Orders.create(
-      {
-        type: 'delivery',
-        latitude: orderData.latitude,
-        longitude: orderData.longitude,
-        code: randomSixDigitNumber,
-        description: orderData.description || '',
-        status: 'started',
-        UserId: orderData.UserId,
-        ProviderId: orderData.ProviderId,
-        ProviderServiceId: orderData.ProviderServiceId,
-      }
-    )
-    return newOrder
-  } catch (error) {
-    throw new Error(error as string)
-  }
-}
